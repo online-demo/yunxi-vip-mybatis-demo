21bd6fd77dcd3567ff652b589a44fb9e846d23eb
zhouguanya <zhouguanya20@163.com>
2020-08-22 11:43:50 +0800

    update

--- src/test/java/com/example/yunxi/mybatis/demo/mapper/TeacherMapperTest.java
+++ src/test/java/com/example/yunxi/mybatis/demo/mapper/TeacherMapperTest.java
@@ -22,7 +22,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = YunxiMybatisDemoApplication.class)
 @Slf4j
-public class StudentMapperTest {
+public class TeacherMapperTest {
     @Autowired
     private SqlSessionFactory factory;
 
